16398b4c00fe7c5a7c0799eeb7a4891e57841466
Robisca <andriesrobert7@gmail.com>
2026-02-11 19:06:51 +0200

    Added api template, enrich it with serilog and added migration functionality

---
+++ GoldenTable.Backend/GoldenTable.Api/Program.cs
@@ -0,0 +1,19 @@
+using GoldenTable.Api.Extensions;
+using Serilog;
+
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+builder.Host.UseSerilog((context, loggerConfig) => loggerConfig.ReadFrom.Configuration(context.Configuration));
+
+WebApplication app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    app.ApplyMigrations();
+}
+
+app.UseHttpsRedirection();
+
+app.Run();
